351f97c78a1087d890b566f34bbfa83c205438f6
zhfreal <zhfreal@gmail.com>
2022-08-12 13:33:59 +0800

    combine exception.py to cloudflare.py for convenience

--- cloudflare.py
+++ cloudflare.py
@@ -8,8 +8,26 @@ import json
 import time
 import urllib.parse
 import requests
-from exceptions import ZoneNotFound, RecordNotFound, CloudFlareError
 
+class CloudFlareError(Exception):
+    """
+    Base exception for CloudFlare module
+    """
+    pass
+
+
+class ZoneNotFound(CloudFlareError):
+    """
+    Raised when a specified zone is not found from CloudFlare
+    """
+    pass
+
+
+class RecordNotFound(CloudFlareError):
+    """
+    Raised when a specified record is not found for a zone from CloudFlare
+    """
+    pass
 
 class CloudFlare:
     """
